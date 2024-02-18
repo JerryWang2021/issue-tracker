60c4acb8957c53758c65105dedb38148625d13c3
Zhi Wang <jerrywang65@hotmail.com>
2024-02-17 17:43:14 -0800

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -18,6 +18,8 @@ import Spinner from '@/app/components/Spinner'
 
  type IssueForm = z.infer<typeof createIssueSchema>
 
+ 
+
 const NewIssuePage = () => {
     const router = useRouter()
     const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
@@ -26,6 +28,17 @@ const NewIssuePage = () => {
     const  [error, setError] = useState('')
     const [isSubmitting, setSubmitting] = useState(false)
 
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true)
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+        } catch (error) {
+            setSubmitting(false)
+            setError('An unexpected error occurred.')
+        }
+    })
+
   return (
     <div className='max-w-xl'>
         {error && <Callout.Root color='red' className='mb-5'>
@@ -33,16 +46,7 @@ const NewIssuePage = () => {
             </Callout.Root>}
         <form 
             className='space-y-3' 
-            onSubmit={handleSubmit(async (data) => {
-                try {
-                    setSubmitting(true)
-                    await axios.post('/api/issues', data)
-                    router.push('/issues')
-                } catch (error) {
-                    setSubmitting(false)
-                    setError('An unexpected error occurred.')
-                }
-        })}>
+            onSubmit={onSubmit}>
             <TextField.Root>
                 <TextField.Input placeholder='Title' {...register('title')}/>
             </TextField.Root>
