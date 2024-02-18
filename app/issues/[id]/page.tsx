4dec66b57a03fad85359dcb08018f639f6851af0
Zhi Wang <jerrywang65@hotmail.com>
2024-02-17 22:38:18 -0800

    Add additional Loading Skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,20 +1,21 @@
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import prisma from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { notFound } from 'next/navigation'
 import ReactMarkdown from 'react-markdown'
 
 interface Props {
     params: {id: string}
 }
 
+
 const IssueDetailPage = async ({params}: Props) => {
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id)}
     })
-
+    
     if (!issue) 
-        notFound()
+    notFound()
 
     return (
         <div>
