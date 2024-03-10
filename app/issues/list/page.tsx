d46cdd4e6d728f9aa007eef98cfac22e327f7be2
Zhi Wang <jerrywang65@hotmail.com>
2024-03-09 17:28:17 -0800

    Fix Filtering Bugs

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -21,8 +21,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
 
-  // const statuses = Object.values(Status)
-  // const status = statuses.includes(searchParams.status) ? searchParams : undefined
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
 
   const orderBy = columns
     .map((column) => column.value)
@@ -32,7 +34,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
   const issues = await prisma.issue.findMany({
     where: {
-      status: searchParams.status,
+      status: status,
     },
     orderBy,
   });
@@ -44,7 +46,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <TableRow>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
